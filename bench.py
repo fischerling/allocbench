f1f2b7ebb57d1d5a985cbded13ac39f0388a1eef
Florian Fischer <florian.fl.fischer@fau.de>
2018-07-20 16:14:23 +0200

    make run argument an integer

--- bench.py
+++ bench.py
@@ -9,7 +9,7 @@ from bench_mysql import mysql
 parser = argparse.ArgumentParser(description="benchmark memory allocators")
 parser.add_argument("-s", "--save", help="save benchmark results to disk", action='store_true')
 parser.add_argument("-l", "--load", help="load benchmark results from disk", action='store_true')
-parser.add_argument("-r", "--runs", help="how often the benchmarks run", default=3)
+parser.add_argument("-r", "--runs", help="how often the benchmarks run", default=3, type=int)
 
 
 benchmarks = [loop, conprod, mysql]
