e29de27e578432762ba4afd225d0331a8c66b7f7
Florian Fischer <florian.fl.fischer@fau.de>
2018-07-23 11:26:50 +0200

    load and save from/to specific file

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,17 @@
 import pickle
 
 class Benchmark (object):
-    def save(self, verbose=False):
+    def save(self, path=None, verbose=False):
+        f = path if path else self.file_name + ".save"
         if verbose:
             print("Saving results to:", self.file_name + ".save")
-        with open(self.file_name + ".save", "wb") as f:
+        with open(f, "wb") as f:
             pickle.dump(self.results, f)
 
-    def load(self, verbose=False):
+    def load(self, path=None, verbose=False):
+        f = path if path else self.file_name + ".save"
         if verbose:
             print("Loading results from:", self.file_name + ".save")
-        with open(self.file_name + ".save", "rb") as f:
+        with open(f, "rb") as f:
             self.results = pickle.load(f)
 
