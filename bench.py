976c6939b1abd4ddf26b03659446517f2956a7bf
Florian Fischer <florian.fl.fischer@fau.de>
2018-07-23 11:29:12 +0200

    add verbose cmdline option

--- bench.py
+++ bench.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description="benchmark memory allocators")
 parser.add_argument("-s", "--save", help="save benchmark results to disk", action='store_true')
 parser.add_argument("-l", "--load", help="load benchmark results from disk", action='store_true')
 parser.add_argument("-r", "--runs", help="how often the benchmarks run", default=3, type=int)
+parser.add_argument("-v", "--verbose", help="more output", action='store_true')
 
 
 benchmarks = [loop, conprod, mysql]
