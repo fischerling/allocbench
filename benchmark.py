05c3559e2dfd015b2e7f2deca7aa9f1692c84ecb
Florian Fischer <florian.fl.fischer@fau.de>
2018-07-24 10:28:20 +0200

    fix save and load

--- benchmark.py
+++ benchmark.py
@@ -2,16 +2,16 @@ import pickle
 
 class Benchmark (object):
     def save(self, path=None, verbose=False):
-        f = path if path else self.file_name + ".save"
+        f = path if path else self.name + ".save"
         if verbose:
-            print("Saving results to:", self.file_name + ".save")
+            print("Saving results to:", self.name + ".save")
         with open(f, "wb") as f:
             pickle.dump(self.results, f)
 
     def load(self, path=None, verbose=False):
-        f = path if path else self.file_name + ".save"
+        f = path if path else self.name + ".save"
         if verbose:
-            print("Loading results from:", self.file_name + ".save")
+            print("Loading results from:", self.name + ".save")
         with open(f, "rb") as f:
             self.results = pickle.load(f)
 
