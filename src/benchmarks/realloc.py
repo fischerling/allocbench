992e64cf93ee0c4312c15c43fdc302c0d3dee2f6
Florian Fischer <florian.fl.fischer@fau.de>
2019-04-15 12:05:41 +0200

    use new barplot function

--- src/benchmarks/realloc.py
+++ src/benchmarks/realloc.py
@@ -19,19 +19,9 @@ class Benchmark_Realloc(Benchmark):
         # bar plot
         allocators = self.results["allocators"]
 
-        for i, allocator in enumerate(allocators):
-            y_vals = []
-            for perm in self.iterate_args(args=self.results["args"]):
-                y_vals.append(self.results["mean"][allocator][perm]["task-clock"])
-            x_vals = [i * x for x in range(1, len(y_vals) + 1)]
-            plt.bar(x_vals, y_vals, width=0.7, label=allocator, align="center",
-                    color=allocators[allocator]["color"])
-        
-        plt.legend()
-        plt.ylabel("task-clock in ms")
-        plt.title("realloc micro bench")
-        plt.savefig(self.name + ".png")
-        plt.clf()
+        self.barplot_single_arg("{task-clock}",
+                                ylabel='"task-clock in ms"',
+                                title='"realloc micro benchmark"')
 
         self.export_to_csv("task-clock")
 
