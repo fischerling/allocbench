af9a1be4d59438bcb24cdd64572841563860b87a
Florian Fischer <florian.fl.fischer@fau.de>
2019-06-24 17:23:22 +0200

    fix all allocators collection

--- src/allocators/all.py
+++ src/allocators/all.py
@@ -5,18 +5,11 @@ import src.allocators.glibcs
 from src.allocators.tcmalloc import tcmalloc, tcmalloc_nofs
 from src.allocators.jemalloc import jemalloc
 from src.allocators.hoard import hoard
+from src.allocators.mesh import mesh
+from src.allocators.scalloc import scalloc
 from src.allocators.supermalloc import supermalloc
 from src.allocators.llalloc import llalloc
 
 
-mesh = Alloc("Mesh", sources=Alloc_Src("Mesh",
-                                       retrieve_cmds=["git clone https://github.com/plasma-umass/Mesh"],
-                                       reset_cmds=["git stash"]),
-                            LD_PRELOAD="{srcdir}/libmesh.so",
-                            build_cmds=["cd {srcdir}; git submodule update --init",
-                                        "cd {srcdir}; ./configure",
-                                        "cd {srcdir}; make -j 4",
-                                        "mkdir {dir}"])
-
 allocators = [*src.allocators.glibcs.allocators, tcmalloc, tcmalloc_nofs,
-              jemalloc, hoard, mesh, supermalloc, llalloc]
+              jemalloc, hoard, mesh, supermalloc, scalloc, llalloc]
