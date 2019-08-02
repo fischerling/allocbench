dee84e370628f0feb54b8b3941dcf806b5b1c13b
Florian Fischer <florian.fl.fischer@fau.de>
2019-08-02 10:38:08 +0200

    load facts and remove threading

--- scripts/bench_sum.py
+++ scripts/bench_sum.py
@@ -4,15 +4,14 @@ import argparse
 import importlib
 import inspect
 import os
+import pickle
 import sys
-import _thread
-import threading
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-from src.globalvars import benchmarks
+import src.globalvars
 
 def specific_summary(bench, set_name, allocators):
     os.mkdir(set_name)
@@ -22,12 +21,16 @@ def specific_summary(bench, set_name, allocators):
         allocators["bumpptr"] = {"color": "C9"}
 
     old_allocs = bench.results["allocators"]
+
+    if bench.name == "mysql" and "Hoard" in allocators:
+        del(allocators["Hoard"])
     bench.results["allocators"] = allocators
     bench.summary()
     bench.results["allocators"] = old_allocs
 
     if bench.name == "loop":
         del(allocators["bumpptr"])
+
 
     os.chdir("..")
 
@@ -65,15 +68,17 @@ parser = argparse.ArgumentParser(description="Summarize allocbench results in al
 parser.add_argument("results", help="path to results", type=str)
 parser.add_argument("-b", "--benchmarks", help="benchmarks to summarize", nargs='+')
 parser.add_argument("-x", "--exclude-benchmarks", help="benchmarks to exclude", nargs='+')
-parser.add_argument("-t", "--threads", help="Summarize using multiple threads", action="store_true")
 
 def main():
     args = parser.parse_args()
     os.chdir(args.results)
 
-    active_threads = []
+    # Load facts
+    with open("facts.save", "rb") as f:
+        src.globalvars.facts = pickle.load(f)
 
-    for b in benchmarks:
+    print(src.globalvars.facts)
+    for b in src.globalvars.benchmarks:
         if args.benchmarks and not b in args.benchmarks:
             continue
         if args.exclude_benchmarks and b in args.exclude_benchmarks:
@@ -83,20 +88,13 @@ def main():
         try:
             bench.load()
         except FileNotFoundError as e:
-            print("No data available")
+            print(bench.name, "No data available")
             continue
         
-        if args.threads:
-            active_threads.append(_thread.start_new_thread(bench_sum, (bench, sets)))
-
-            for thread in threading.enumerate():
-                if thread is not threading.main_thread():
-                    thread.join()
-        else:
-            try:
-                bench_sum(bench, sets)
-            except FileExistsError as e:
-                print(e)
+        try:
+            bench_sum(bench, sets)
+        except FileExistsError as e:
+            print(e)
 
 
 if __name__ == "__main__":
