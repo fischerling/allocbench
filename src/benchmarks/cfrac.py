77ac9ce0a5c55d4f79f8fb8f7daa59ddb53cb507
Florian Fischer <florian.fl.fischer@fau.de>
2019-08-24 17:57:51 +0200

    add cfrac benchmark

---
+++ src/benchmarks/cfrac.py
@@ -0,0 +1,32 @@
+from src.benchmark import Benchmark
+
+class Benchmark_Cfrac(Benchmark):
+    def __init__(self):
+        self.name = "cfrac"
+        self.descrition = """TODO."""
+
+        self.cmd = "cfrac{binary_suffix} {num}"
+
+        self.args = {"num": [175451865205073170563711388363274837927895]}
+
+        self.requirements = ["cfrac"]
+        super().__init__()
+
+    def summary(self):
+        # Speed
+        self.barplot_single_arg("{task-clock}/1000",
+                                ylabel='"cpu-second"',
+                                title='"Cfrac: runtime"',
+                                filepostfix="time")
+
+        # L1 cache misses
+        self.barplot_single_arg("({L1-dcache-load-misses}/{L1-dcache-loads})*100",
+                                ylabel='"L1 misses in %"',
+                                title='"Cfrac l1 cache misses"',
+                                filepostfix="l1misses")
+
+        self.export_stats_to_csv("task-clock")
+        self.export_stats_to_dataref("task-clock")
+
+
+cfrac = Benchmark_Cfrac()
