ed3c92e2c0bf209f6e7b67ad3a715529aad2a9d1
Florian Fischer <florian.fl.fischer@fau.de>
2019-08-27 14:48:35 +0200

    add infos about allocbench to src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+# Copyright 2018-2019 Florian Fischer <florian.fl.fischer@fau.de>
+#
+# This file is part of allocbench.
+#
+# allocbench is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# allocbench is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with allocbench.  If not, see <http://www.gnu.org/licenses/>.
+
+"""A framework linking allocators to applications"""
+
+__author__ = "Florian Fischer"
+__copyright__ = "Copyright 2018-2019 Florian Fischer"
+__license__ = "GPL"
+__maintainer__ = __author__
+__email__ = "florian.fl.fischer@fau.de"
+__description__ = "A framework linking allocators to applications"
