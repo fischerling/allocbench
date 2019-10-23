a0bac24b506d158f9ae09e0532a07546ead4d0ce
Florian Fischer <florian.fl.fischer@fau.de>
2019-10-23 15:26:36 +0200

    use new dj_workload location

--- src/benchmarks/dj_trace.py
+++ src/benchmarks/dj_trace.py
@@ -58,7 +58,7 @@ class BenchmarkDJTrace(Benchmark):
     def __init__(self):
         name = "dj_trace"
 
-        self.cmd = "trace_run{binary_suffix} {build_dir}/dj_workloads/{workload}.wl"
+        self.cmd = "trace_run{binary_suffix} {build_dir}/dj_workloads/dj_workloads/{workload}.wl"
         self.measure_cmd = ""
 
         self.args = {"workload": ["389-ds-2",
@@ -111,7 +111,7 @@ class BenchmarkDJTrace(Benchmark):
                                     "https://www4.cs.fau.de/~flow/allocbench/dj_workloads.tar.xz",
                                     "tar",
                                     "c9bc499eeba8023bca28a755fffbaf9200a335ad")
-        workloads.provide(os.path.join(self.build_dir, "dj_trace"))
+        workloads.provide(os.path.join(self.build_dir, "dj_workloads"))
 
     @staticmethod
     def process_output(result, stdout, stderr, allocator, perm):
