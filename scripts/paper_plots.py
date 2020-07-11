5f60c2fd87fd4cf8aed7b98633ad946367f58b69
Florian Fischer <florian.fl.fischer@fau.de>
2020-07-12 00:58:01 +0200

    [scripts/paper_plots] fix mypy error

--- scripts/paper_plots.py
+++ scripts/paper_plots.py
@@ -20,12 +20,10 @@
 
 import argparse
 import importlib
-import inspect
 import os
 import sys
 
-currentdir = os.path.dirname(
-    os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
